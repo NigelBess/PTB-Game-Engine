539a418f4b355d50ddea9ce93d5ee41197b60b24
jproney <nigelbess@gmail.com>
2019-06-06 13:24:46 -0700

    renderable updated for gpu instancing. TODO: renderer and physics object

--- GameEngine/Renderable.m
+++ GameEngine/Renderable.m
@@ -11,17 +11,39 @@ classdef Renderable < GameObject
         renderLayer = 100
     end
     methods (Access = public)
-        function [positions,tex] = GetData(obj)
+        function obj = InstantiateNew(obj,position,size)
+            if nargin<2
+                position = [0,0];
+            end
+            if nargin<3
+                size = [1,1];
+            end
+            obj.position(end+1,:) = position;
+            obj.size(end+1,:) = size;
+            obj.screenHits(end+1,:) = zeros(1,2);
+        end
+          function obj = Remove(obj,instance)
+            obj.position(instance,:) = [];
+            obj.size(instance,:) = [];
+            obj.screenHits(instance,:) = [];
+        end
+        function [positions,tex] = GetData(obj,instance)
+            if nargin<2
+                instance = 1;
+            end
             c = obj.Renderer.Center();
-            positions = [obj.position(1)-obj.size(1)/2 ,obj.position(2)-obj.size(2)/2,obj.position(1)+obj.size(1)/2,obj.position(2)+obj.size(2)/2];
+            positions = [obj.position(instance,1)-obj.size(instance,1)/2 ,obj.position(instance,2)-obj.size(instance,2)/2,obj.position(instance,1)+obj.size(instance,1)/2,obj.position(instance,2)+obj.size(instance,2)/2];
            positions = positions + [c,c];
             tex = obj.texture;
         end
-        function obj = SetPosition(obj,pos)
-            obj.position = pos;
+        function obj = SetPosition(obj,pos,instance)
+             if nargin<3
+                instance = 1;
+            end
+            obj.position(instance,:) = pos;
             if obj.screenBounded
-                hits = obj.Renderer.CheckSideIntersection(obj);
-                obj.screenHits = hits;
+                hits = obj.Renderer.CheckSideIntersection(obj,instance);
+                obj.screenHits(instance,:) = hits;
             else
                 return;
             end
@@ -32,13 +54,16 @@ classdef Renderable < GameObject
                         index = (hits(i)+3)/2;
                         c = obj.Renderer.Center;
                         rect(4) =0;
-                        obj.position(i) = rect(i*index) - hits(i)*(obj.size(i)/2-c(i));
+                        obj.position(instance,i) = rect(i*index) - hits(i)*(obj.size(instance,i)/2-c(i));
                     end
                 end
             end
         end
-        function out = GetPosition(obj)
-            out = obj.position;
+        function out = GetPosition(obj,instance)
+             if nargin<2
+                instance = 1;
+            end
+            out = obj.position(instance,:);
         end
         function img = GenerateImage(obj)
         end
@@ -48,15 +73,21 @@ classdef Renderable < GameObject
         function obj = SendImage(obj)
             obj.texture = obj.Renderer.ImageToTexture(obj.image);
         end
-        function out = GetScreenHits(obj,index)
-            out = obj.screenHits;
+        function out = GetScreenHits(obj,index,instance)
+             if nargin<3
+                instance = 1;
+            end
+            out = obj.screenHits(instance,:);
             if nargin>1
                 out = out(index);
             end
         end
-         function out = Distance(obj,other,index)
-            pos1 = obj.GetPosition;
-            pos2 = other.GetPosition;
+         function out = Distance(obj,other,index,instance)
+            if nargin<4
+                instance = 1;
+            end
+            pos1 = obj.GetPosition(instance);
+            pos2 = other.GetPosition();
             if nargin<3
                 for i = 1:2
                     out = sqrt(sum((pos1-pos2).^2));
